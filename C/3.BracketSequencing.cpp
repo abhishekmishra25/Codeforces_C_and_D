472b16b039659b562dad8a67c6de3c79b4675fad
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2022-07-12 23:37:10 +0530

    added

--- C/3.BracketSequencing.cpp
+++ C/3.BracketSequencing.cpp
@@ -7,9 +7,46 @@ public:
     string BracketSequencing(string s, int n, int k)
     {
         string ans = "";
+        int count = 0;
+        int *arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = 0;
+        stack<int> st;
+        for (int i = 0; i < n; i++)
+        {
+            if (count == k)
+                if (s[i] == '(')
+                    st.push(i);
+                else
+                {
+                    if (st.empty())
+                        st.push(i);
+                    else
+                    {
+                        arr[i] = 1;
+                        int k = st.top();
+                        arr[k] = 1;
+                        st.pop();
+                        count += 2;
+                    }
+                }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] != 0)
+                ans += s[i];
+        }
+        return ans;
     }
 };
 
 int main()
 {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    solution ob1;
+    cout << ob1.BracketSequencing(s, n, k);
+    return 0;
 }
