20d46e3f445dd553d7190f333f18cb2c9c236fba
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2022-08-15 11:07:45 +0530

    updated

---
+++ contest 811/ab.cpp
@@ -0,0 +1,71 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+#define ins(i, a, n) for (int i = a; i < n; i++)
+#define des(i, a, n) for (int i = n - 1; i >= a; i--)
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define pqueue priority_queue
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e18;
+const int N = 1e5 + 5;
+#define print(a) cout << a << endl
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p)
+{
+    return os << '(' << p.first << ", " << p.second << ')';
+}
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define lli long long int
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fo(i, n) for (int k = i; k < n; k++)
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int main()
+{
+    int a;
+    cin >> a;
+    vector<vector<int>> arr(a, vector<int>(a, 0));
+    ins(i, 0, a)
+    {
+        ins(j, 0, a)
+        {
+            cin >> arr[i];
+        }
+    }
+    vector<bool> dp(a + 1, false);
+    dp[0] = true;
+}
